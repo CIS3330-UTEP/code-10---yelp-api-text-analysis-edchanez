11ebfb20062e93c83409fc94012cb345729e6707
edchanez <edchanez@gmail.com>
2023-04-13 21:17:22 -0600

    code 10

--- code_10.py
+++ code_10.py
@@ -3,14 +3,14 @@ import pandas as pd
 from yelpapi import YelpAPI
 from nltk.corpus import stopwords
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-# nltk.download('stopwords')
+
 
 api_key = "r_PCQETtPYslevCCq0RBsvxh6rHtnGbO7yfGlUf451AKoAYAQ5hTW82lNR4jWkGOuKuEm1BYFWmbyMNdl1uQoSh3tmtU3RaroUnvEWBlMovUT79iSXjKlqaoB60zZHYx"
 
 yelp_api = YelpAPI(api_key)
 
 #search query
-search_term = "pasta"
+search_term = "coffee"
 search_location = "El Paso, TX"
 search_sort_by = "rating" #best_match, rating, review_count, distance
 search_limit = 20
@@ -21,7 +21,10 @@ print(search_results)
 result_df = pd.DataFrame.from_dict(search_results['businesses'])
 print(result_df['alias'])
 # result_df.to_csv("yelpAPI_TEST.csv")
-id_for_reviews = "buon-giorno-caffe-el-paso"
+id_for_reviews = "ambers-coffee-bar-horizon-city"
+
+
+
 
 
 reviews_result = yelp_api.reviews_query(id=id_for_reviews)
@@ -38,29 +41,30 @@ for review in reviews_df['text']:
     tokens = nltk.word_tokenize(review)
     pos_tags = nltk.pos_tag(tokens)
     for tag in pos_tags:
-        if tag[1] == 'JJ' or tag[1] == 'JJS':
+        if tag[1] == 'JJ' or tag[1] == 'JJS' or tag[1] == 'NN':
             print(tag[0])
     print(pos_tags)   
 
+#common adjectives and nouns for each coffee shop
+# id_for_reviews = "2ten-coffee-roasters-el-paso" #friendly, attentive, service, atmosphere, location, tasty
+# id_for_reviews = "viejo-coffee-el-paso-4" #little, spot, minimalist, modern, vibes, latte, horchata
+# id_for_reviews = "café-con-leche-el-paso-6" #atmosphere, coffee, artsy, latte, hidden gem
+# id_for_reviews = "lucys-coffee-shop-el-paso" #small, breakfast, great service, kitchen, food
+# id_for_reviews = "ambers-coffee-bar-horizon-city" #pic, sweet staff,local, adorable, caramel, macchiato, good atmosphere
+
+
 #sentiment
-# analyzer = SentimentIntensityAnalyzer()
+analyzer = SentimentIntensityAnalyzer()
 
-# for review in reviews_df['text']:
-#     sentiment_score = analyzer.polarity_scores(review)
-#     print(review)
-#     print(sentiment_score)
-#     print("\n")
+for review in reviews_df['text']:
+    sentiment_score = analyzer.polarity_scores(review)
+    print(review)
+    print(sentiment_score)
+    print("\n")
 
-#stop words
-stop_words = set(stopwords.words('english'))
-
-new_text = []
-for tag in pos_tags:
-    if tag[0] not in stop_words:
-        print(tag[0])
-        new_text.append(tag[0])
-
-print("\nOriginal")
-print(review)
-print("\nNew")
-print(" ".join(new_text))
+#average sentiment scores
+# id_for_reviews = "2ten-coffee-roasters-el-paso" avg 'neg' score:0.026, avg 'neu'score:0.738, avg 'pos' score: 0.236
+# id_for_reviews = "viejo-coffee-el-paso-4" avg 'neg' score:0.044, avg 'neu'score:0.758, avg 'pos' score:0.197
+# id_for_reviews = "café-con-leche-el-paso-6" avg 'neg' score:0.0, avg 'neu'score:0.717, avg 'pos' score: 0.85
+# id_for_reviews = "lucys-coffee-shop-el-paso" avg 'neg' score:0.0, avg 'neu'score:0.762, avg 'pos' score: 0.713
+# id_for_reviews = "ambers-coffee-bar-horizon-city" avg 'neg' score:0.032, avg 'neu'score:0.577, avg 'pos' score:0.391
